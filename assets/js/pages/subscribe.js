023730ca8d90391e4959ca5d370be4bc46280082
emiel vandewalle <emiel.vandewalle@student.howest.be>
2023-12-16 20:27:46 +0100

    change subscribe.js

--- assets/js/pages/subscribe.js
+++ assets/js/pages/subscribe.js
@@ -4,11 +4,12 @@
 
 function initSubscribePage(){
     document.querySelector("input[type=submit]").addEventListener("click", goToBoostersPage);
-    document.querySelector(" #subscribe form").addEventListener("submit", submitForm);
+    document.querySelector("#subscribe form").addEventListener("submit", submitForm);
     renderSet(config.sets);
 }
 
 // ## YOUR ADDED FUNCTIONS ##
+
 let _name;
 let _set;
 let _booster;
@@ -20,20 +21,21 @@ function goToBoostersPage() {
 function submitForm(e) {
     e.preventDefault();
 
-    const $select = document.querySelector("#subscribe form select");
-    _name = $select.value;
-    _set = $select.value;
-    _booster = $select.value;
+    console.log("triggers");
+
+    const $subForm = document.querySelector("#subscribe form");
+    _name = $subForm.querySelector('input[name="name"]').value;
+    _set = $subForm.querySelector('input[name="choice"]:checked').value;
+    _booster = $subForm.querySelector('input[name="boosters"]').value;
 
     navigateToPage("boosters");
 }
 
 function renderSet(sets) {
-    const $set = document.querySelector("#subscribe form > label:nth-child(7)");
+    const $set = document.querySelector("#set");
     $set.innerHTML = "";
-    $set.insertAdjacentHTML("beforeend", `<option></option>`);
     for(const set in sets) {
-        $set.insertAdjacentHTML("beforeend", `<input type="radio" id="${set}" name="choice" checked="checked"> <label> ${sets[set]} </label>
+        $set.insertAdjacentHTML("beforeend", `<input type="radio" id="${set}" name="choice" value="${set}"> <label> ${sets[set]} </label>
 `)
     }
 }
